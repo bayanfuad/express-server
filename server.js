772f0adc9f4d97758524ab3418fc5e35f9c7a369
bayanfuad <bayanjawawdeh9@gmail.com>
2022-09-05 20:09:15 +0300

    fix with front

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 'use strict';
 const express = require('express');
 const app=express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 
 app.get('/',(req,res)=>res.status(200).send('Hello World'));
 
-app.post('/person',(req,res)=>{
-    const {name,age,gender}=req.query;
-    let newAge=parseInt(age)+5;
-    res.status(201).send({name:name , gender:gender , age:`after 5 years you will be ${newAge}`})
+app.post('/person',(req, res)=>{
+    const {age} = req.body;
+    let parsedAge = parseInt(age)+5;
+    console.log( typeof res)
+    res.status(201).send(`${parsedAge}`);
 })
 
 function start (port){
@@ -17,3 +21,9 @@ module.exports={
    app: app,
    start: start
 }
+
+
+// app.post('/person',(req,res)=>{
+//     const {name,age,gender}=req.query;
+//     let newAge=parseInt(age)+5;
+//     res.status(201).send({name:name , gender:gender , age:`after 5 years you will be ${newAge}`})
